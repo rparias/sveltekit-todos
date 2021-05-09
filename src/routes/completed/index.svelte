c40d1d7b3537dbee5d2db23e86b2fcac270ede52
Ronald Arias <ariasron@hotmail.com>
2021-05-09 14:20:54 -0500

    Display completed todos

--- src/routes/completed/index.svelte
+++ src/routes/completed/index.svelte
@@ -1,1 +1,25 @@
+<script context="module">
+	export async function load({ fetch }) {
+		const res = await fetch('/todos?completed=true');
+		const jsonRes = await res.json();
+		return {
+			props: {
+				todos: jsonRes.todos
+			}
+		};
+	}
+</script>
+
+<script>
+	export let todos;
+</script>
+
 <h1>Completed Todos</h1>
+<ul>
+	{#each todos as todo}
+		<li>
+			<input type="checkbox" bind:checked={todo.completed} disabled />
+			{todo.text}
+		</li>
+	{/each}
+</ul>
