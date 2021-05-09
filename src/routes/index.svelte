b98284d8730d954f470a30baf6013bc5bd1d328c
Ronald Arias <ariasron@hotmail.com>
2021-05-09 13:58:06 -0500

    Saves a todo on database

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-	documentation
-</p>
+<script>
+	let text = '';
+
+	async function addTodo() {
+		try {
+			const todo = {
+				text,
+				completed: false
+			};
+			await fetch('/todos', {
+				method: 'POST',
+				body: JSON.stringify(todo)
+			});
+			alert('Saved successfully');
+			text = '';
+		} catch (err) {
+			alert('There was an error');
+			text = '';
+		}
+	}
+</script>
+
+<main>
+	<h1>My Todos</h1>
+	<input type="text" bind:value={text} placeholder="Enter a todo" />
+	<button on:click={addTodo}>Add Todo</button>
+</main>
--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -2,30 +2,53 @@ import { connectToDatabase } from '$lib/db';
 // import { ObjectId } from 'mongodb';
 
 export async function get(request) {
-	try {
-		const completed = request.query.get('completed') === 'true';
+  try {
+    const completed = request.query.get('completed') === 'true';
 
-		const dbConnection = await connectToDatabase();
-		const db = dbConnection.db;
-		const collection = db.collection('sveltekit-todos');
-		const todos = await collection.find({ completed }).toArray();
+    const dbConnection = await connectToDatabase();
+    const db = dbConnection.db;
+    const collection = db.collection('sveltekit-todos');
+    const todos = await collection.find({ completed }).toArray();
 
-		return {
-			status: 200,
-			body: {
-				todos
-			}
-		};
-	} catch (err) {
-		return {
-			status: 500,
-			body: {
-				error: 'A server error occured'
-			}
-		};
-	}
+    return {
+      status: 200,
+      body: {
+        todos,
+      },
+    };
+  } catch (err) {
+    return {
+      status: 500,
+      body: {
+        error: 'A server error occured',
+      },
+    };
+  }
 }
 
-// export async function post(request) {}
+export async function post(request) {
+  try {
+    const dbConnection = await connectToDatabase();
+    const db = dbConnection.db;
+    const collection = db.collection('sveltekit-todos');
+
+    const todo = JSON.parse(request.body);
+    await collection.insertOne(todo);
+
+    return {
+      status: 200,
+      body: {
+        status: 'Success',
+      },
+    };
+  } catch (err) {
+    return {
+      status: 500,
+      body: {
+        error: 'A server error occured',
+      },
+    };
+  }
+}
 // export async function put(request) {}
 // export async function del(request) {}
