0971c0fcd1f785f35cd5d5b3bc331cdfd5a5384f
Ronald Arias <ariasron@hotmail.com>
2021-05-10 22:09:27 -0500

    Deletes vite config from svelte.config and added node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import preprocess from 'svelte-preprocess';
-import pkg from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
@@ -9,13 +8,7 @@ const config = {
 	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-			ssr: {
-				external: Object.keys(pkg.dependencies || {})
-			}
-		}
+		target: '#svelte'
 	}
 };
 
